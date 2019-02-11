2d40b866f0a314912417c5a95abbbe4735d7c431
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-02-11 16:22:19 -0500

    Updated clock component so that real-time is displayed and updated every second

--- magic-mirror/src/app/app.module.ts
+++ magic-mirror/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
+import { WeatherComponent } from './weather/weather.component';
+import { MotivationComponent } from './motivation/motivation.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { UcfRedditComponent } from './ucf-reddit/ucf-reddit.component';
+import { TravelTimesComponent } from './travel-times/travel-times.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClockComponent
+    ClockComponent,
+    WeatherComponent,
+    MotivationComponent,
+    CalendarComponent,
+    UcfRedditComponent,
+    TravelTimesComponent
   ],
   imports: [
     BrowserModule
--- magic-mirror/src/app/clock/clock.component.ts
+++ magic-mirror/src/app/clock/clock.component.ts
@@ -6,13 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clock.component.css']
 })
 export class ClockComponent implements OnInit {
+  date: Date = new Date();
+  hours: number = this.date.getHours() % 12;
+  minutes: number = this.date.getMinutes();
+  seconds: number = this.date.getSeconds();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    // update time every second
+    setInterval(() => {
+      this.updateTime();
+    },1000);
   }
 
+  ngOnInit() {
 
+  }
 
-
+  updateTime() {
+    this.date = new Date();
+    this.hours = this.date.getHours() % 12;
+    this.minutes = this.date.getMinutes();
+    this.seconds = this.date.getSeconds();
+  }
 }

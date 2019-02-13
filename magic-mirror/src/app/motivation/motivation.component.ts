932b932f7d96bbd65f9e15648be6b305bc20130d
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-02-13 18:31:48 -0500

    Implemented basic functionality to motivation component, updated root styling

--- magic-mirror/src/app/motivation/motivation.component.ts
+++ magic-mirror/src/app/motivation/motivation.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motivation.component.css']
 })
 export class MotivationComponent implements OnInit {
+  quotes: string[] = ['You got this!', 'You are beautiful and or handsome', 'yup'];
+  currentQuote: string;
 
-  constructor() { }
+
+  constructor() {
+    this.currentQuote = this.quotes[0];
+
+    setInterval( () => {
+      this.getQuote();
+    }, 8000);
+  }
 
   ngOnInit() {
+  }
+
+  getQuote() {
+      this.quotes.unshift( this.quotes.pop() );
+      this.currentQuote = this.quotes[0];
   }
 
 }

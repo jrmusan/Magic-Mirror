3b4e47ddb75d47816a178b4fc25fd7698d305986
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-03-02 18:28:11 -0500

    Added calendar functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
-import { WeatherComponent } from './weather/weather.component';
+//import { WeatherComponent } from './weather/weather.component';
 import { MotivationComponent } from './motivation/motivation.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { UcfRedditComponent } from './ucf-reddit/ucf-reddit.component';
 import { TravelTimesComponent } from './travel-times/travel-times.component';
 import { DateComponent } from './date/date.component';
+import { EventItemComponent } from './calendar/event-list/event-item/event-item.component';
+import { EventListComponent } from './calendar/event-list/event-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClockComponent,
-    WeatherComponent,
+    //WeatherComponent,
     MotivationComponent,
     CalendarComponent,
     UcfRedditComponent,
     TravelTimesComponent,
-    DateComponent
+    DateComponent,
+    EventItemComponent,
+    EventListComponent
   ],
   imports: [
     BrowserModule

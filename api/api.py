e1ee15564a7f61cd67d1d163db14e255ebba6217
Sidney Packer <sidp@knights.ucf.edu>
2019-03-16 23:26:13 -0400

    Adjust api keys, utilize constants

--- api/api.py
+++ api/api.py
@@ -5,17 +5,19 @@ import config
 from flask import Flask
 from functools import reduce
 from weather_helpers import extract_weather_data
+from Traffic import extract_travel_time
 
 app = Flask(__name__)
 
 ORLANDO_COORD_STR = '28.5383,-81.3792'
 WEATHER_QUERY_PARAMS = '?exclude=minutely,daily,alerts,flags'
+TRAFFIC_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+TRAFFIC_SOURCE = '28.5383,-81.3792'
+TRAFFIC_DEST = '28.597240,-81.203796'
 
 @app.route("/api/weather")
 def weather():
     r = requests.get('https://api.darksky.net/forecast/{}/{}{}'.format(config.darksky_key, ORLANDO_COORD_STR, WEATHER_QUERY_PARAMS))
-
-    # TODO: Error checking
     return json.dumps(extract_weather_data(r.json()))
 
 @app.route("/api/news")
@@ -28,13 +30,5 @@ def reddit():
     
 @app.route("/api/traffic")
 def traffic():
-    # Hardcode home
-    source = '28.5383,-81.3792'      
-    # Hardcode destination
-    dest = '28.597240,-81.203796'
-    # url variable store url  
-    url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
-    r = requests.get(url + 'origins=' + source +'&destinations=' + dest +'&key=' + api_key) 
-    data = r.json()
-    travelTime = data['rows'][0]['elements'][0]['duration']['text']
-    return json.dumps(travelTime)
+    r = requests.get('{}origins={}&destinations={}&key={}'.format(TRAFFIC_URL, TRAFFIC_SOURCE, TRAFFIC_DEST, config.maps_key)) 
+    return json.dumps(extract_travel_time(r.json()))
